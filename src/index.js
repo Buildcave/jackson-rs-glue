2974c6c0a25e6e5de33f0b28b5494d604d4a3adb
Trevor Alfstad <trevor@buildcave.com>
2017-07-05 16:26:15 -0700

    feat: build:watch will rebuild webpack and allow nodemon to auto load the new stuff in. https://app.asana.com/0/378076363562939/379396205159463

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import _ from 'lodash';
 import express from 'express';
 import RedirectTemplate from './templates/dist/redirect.template';
+// import MapRequestsToUUID from '../lib/map-requests-to-uuid';
+
+const app = express();
+// const redirectHost = 'https://github.com';
 
 // Elasticbeanstalk sets port to upstream port
 // automatically.
 const port = process.env.PORT || 3000;
 
-const app = express();
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+  // console.log(`HEfY TREV: ${redirectHost}${req.path}`);
   // TODO: get the uuid from the request and pass it into the template
+  // MapRequestsToUUID.mapReqByUrl(req)
+  //   .then(() => {
   const compiled = _.template(RedirectTemplate);
-  const response = compiled({ test: 'TEST TEMPLATE VALUEEE' });
+  const response = compiled({ test: 'TESTs123 TEMPLATE VALUEEE' });
   res.send(response);
+    // })
+    // .catch(() => {
+    //   // TODO: forward to same url but with github.com host
+    //   res.redirect(`${redirectHost}${req.path}`);
+    // });
 });
 
 app.listen(port, () => {
